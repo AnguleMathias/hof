e41b88f739ace1157dd3e3afe2ab2f31af0dbfcb
victorjambo <victorjambo@live.com>
2019-02-12 19:03:57 +0300

    :sparkles: :boom: :fire: :octocat:

---
+++ src/Config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyBY7RVdaAor9VUIyZT7e9hH-5xTrRYPj9c",
+  authDomain: "hof-timer.firebaseapp.com",
+  databaseURL: "https://hof-timer.firebaseio.com",
+  projectId: "hof-timer",
+  storageBucket: "hof-timer.appspot.com",
+  messagingSenderId: "1092468910328"
+};
