54b1fadf47b9c442674045425aa7d48005429694
victorjambo <victorjambo@live.com>
2019-02-18 11:54:43 +0300

    Keys

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,8 @@
 export const config = {
-  // TODO: Firebase configuration from web config
+  apiKey: "AIzaSyBY7RVdaAor9VUIyZT7e9hH-5xTrRYPj9c",
+  authDomain: "hof-timer.firebaseapp.com",
+  databaseURL: "https://hof-timer.firebaseio.com",
+  messagingSenderId: "1092468910328",
+  projectId: "hof-timer",
+  storageBucket: "hof-timer.appspot.com"
 };
